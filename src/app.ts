b9ddaaa1267abe3732c6eac22fde8289d5d80589
Risky Nugraha <muhammadrisky83@gmail.com>
2021-09-28 21:15:10 +0700

    use main function to control db & app initiate

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
-const express = require('express');
+import express from 'express';
+import { ConnectionOptions, createConnection } from 'typeorm';
+import config_db from './database/ormconfig';
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World'));
 
-app.listen(PORT, () => console.log('Server Listening on PORT', PORT))
+async function main() {
+  if (process.env.TS_NODE_DEV) {
+    await createConnection(config_db);
+    console.log('DB Has Connected');
+  }
+  
+  app.listen(PORT, () => console.log('Server Listening on PORT', PORT));
+}
+
+main();
