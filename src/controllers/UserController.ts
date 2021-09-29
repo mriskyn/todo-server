15f23544efb26b7af348ca113704f777e7563f52
Risky Nugraha <muhammadrisky83@gmail.com>
2021-09-30 00:51:19 +0700

    add login feature

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import { compare, compareSync } from 'bcrypt';
 import { Request, Response } from 'express';
 import { User } from '../entity/User';
 
@@ -22,4 +23,23 @@ async function register(req: Request, res: Response): Promise<Response> {
   }
 }
 
-export { register };
+async function login(req: Request, res: Response): Promise<Response> {
+  const inputs = req.body;
+
+  try {
+    let user = await User.findOne({ where: { username: inputs.username } });
+    const checkPassword = compareSync(inputs.password, user.password);
+
+    if (!checkPassword || !user) {
+      return res.status(400).json({ msg: 'Wrong Username / Password' });
+    }
+
+    delete user.password;
+    
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+}
+
+export { register, login };
