cd1dfee53d220ee4c85bb113e55a398c0f7ed284
Risky Nugraha <muhammadrisky83@gmail.com>
2021-09-30 00:00:18 +0700

    add /api endpoint and connection name to log

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 import express from 'express';
-import { ConnectionOptions, createConnection } from 'typeorm';
+import { createConnection } from 'typeorm';
 import config_db from './database/ormconfig';
+import router from './routers';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Hello World'));
+app.use('/api', router);
 
 async function main() {
   if (process.env.TS_NODE_DEV) {
-    await createConnection(config_db);
-    console.log('DB Has Connected');
+    const connection = await createConnection(config_db);
+    console.log('DB Has Connected', connection.name,);
   }
-  
+
   app.listen(PORT, () => console.log('Server Listening on PORT', PORT));
 }
 
