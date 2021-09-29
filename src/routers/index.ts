243951a1b3a454f6cb103e5d3075278e9b05f0f4
Risky Nugraha <muhammadrisky83@gmail.com>
2021-09-30 00:50:42 +0700

    add login routing

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,16 +1,12 @@
 import express from 'express';
 import { checkSchema } from 'express-validator';
-import { register } from '../controllers/UserController';
+import { login, register } from '../controllers/UserController';
 import { handleValidationError } from '../middlewares/handleError';
-import { checkRegister } from '../validator/user';
+import { checkLogin, checkRegister } from '../validator/user';
 
 const router = express.Router();
 
-router.post(
-  '/register',
-  checkRegister(),
-  handleValidationError,
-  register
-);
+router.post('/register', checkRegister(), handleValidationError, register);
+router.post('/login', checkLogin(), handleValidationError, login);
 
 export default router;
