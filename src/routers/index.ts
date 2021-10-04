d93e3e78934b709fa5ddf1e43d95697526327faf
Risky Nugraha <muhammadrisky83@gmail.com>
2021-10-04 17:58:14 +0700

    add CRUD task in routers

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
-import { checkSchema } from 'express-validator';
+import {
+  createTask,
+  deleteTask,
+  readTasks,
+  updateTask,
+  readTask,
+} from '../controllers/TaskController';
 import { login, register } from '../controllers/UserController';
 import { handleValidationError } from '../middlewares/handleError';
 import { checkLogin, checkRegister } from '../validator/user';
@@ -8,5 +14,11 @@ const router = express.Router();
 
 router.post('/register', checkRegister(), handleValidationError, register);
 router.post('/login', checkLogin(), handleValidationError, login);
+
+router.post('/task', createTask);
+router.get('/task', readTasks);
+router.get('/task/:id', readTask);
+router.patch('/task/:id', updateTask);
+router.delete('/task/:id', deleteTask);
 
 export default router;
