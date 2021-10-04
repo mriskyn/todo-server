d93ac3a7adafbc349563f929f12c0e7efba03f21
Risky Nugraha <muhammadrisky83@gmail.com>
2021-10-04 19:02:19 +0700

    add authorization middleware

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -7,6 +7,7 @@ import {
   readTask,
 } from '../controllers/TaskController';
 import { login, register } from '../controllers/UserController';
+import { authorization } from '../middlewares/auth';
 import { handleValidationError } from '../middlewares/handleError';
 import { checkLogin, checkRegister } from '../validator/user';
 
@@ -15,6 +16,7 @@ const router = express.Router();
 router.post('/register', checkRegister(), handleValidationError, register);
 router.post('/login', checkLogin(), handleValidationError, login);
 
+router.use(authorization);
 router.post('/task', createTask);
 router.get('/task', readTasks);
 router.get('/task/:id', readTask);
