6c3badb6b0a11dffb62273d513c9e251683f3b65
Risky Nugraha <muhammadrisky83@gmail.com>
2021-10-04 18:25:54 +0700

    create and read task methods

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,16 +1,36 @@
 import { Request, Response } from 'express';
+import { Task } from '../entity/Task';
 
 async function readTasks(req: Request, res: Response): Promise<Response> {
-  return res.json('reads');
+  try {
+    const tasks = await Task.find();
+    return res.json(tasks);
+  } catch (err) {
+    return res.json({ err });
+  }
 }
 
 async function readTask(req: Request, res: Response): Promise<Response> {
-  return res.json('read');
+  const { id } = req.params;
+  try {
+    const task = await Task.findByIds([id]);
+    return res.json(task);
+  } catch (err) {
+    return res.json({ err });
+  }
 }
 
 async function createTask(req: Request, res: Response): Promise<Response> {
   const inputs = req.body;
-  return res.json('create');
+
+  const task = Task.create(inputs);
+
+  try {
+    await Task.save(task);
+    return res.json(inputs);
+  } catch (err) {
+    return res.json({ err });
+  }
 }
 
 async function updateTask(req: Request, res: Response): Promise<Response> {
